8c5910eaf86f1f82dbfbe4e46db2c88812126252
Soaib-Shaikh <samshaikh1192@gmail.com>
2025-07-05 13:10:27 +0530

    update

--- js/index.js
+++ js/index.js
@@ -20,37 +20,39 @@ let displayProducts = (() => {
 
         col.innerHTML =
             `
-                    <div class="product-item">
-                        <span class="badge bg-success position-absolute m-3">-30%</span>
-                        <a href="#" class="btn-wishlist"><svg width="24" height="24"><use xlink:href="#heart"></use></svg></a>
-                        <figure>
-                          <a href="index.html" title="Product Title">
-                            <img src="${image}"  class="tab-image">
-                          </a>
-                        </figure>
-                        <h3>${name}</h3>
-                        <span class="qty">1 Unit</span><span class="rating"><svg width="24" height="24" class="text-primary"><use xlink:href="#star-solid"></use></svg> ${rating}</span>
-                        <span class="price">${price}</span>
-                        <div class="d-flex align-items-center justify-content-between">
-                          <div class="input-group product-qty">
-                              <span class="input-group-btn">
-                                  <button type="button" class="quantity-left-minus btn btn-danger btn-number" data-type="minus">
-                                    <svg width="16" height="16"><use xlink:href="#minus"></use></svg>
-                                  </button>
-                              </span>
-                              <input type="text" id="quantity" name="quantity" class="form-control input-number" value="1">
-                              <span class="input-group-btn">
-                                  <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus">
-                                      <svg width="16" height="16"><use xlink:href="#plus"></use></svg>
-                                  </button>
-                              </span>
-                          </div>
-                          <button type="button" onclick="handleAddtoCart(${id})" class="nav-link">Add to Cart <iconify-icon icon="uil:shopping-cart"></button>
-                        </div>
+                <div class="product-item">
+                    <span class="badge bg-success position-absolute m-3">-30%</span>
+                    <a href="#" class="btn-wishlist"><svg width="24" height="24"><use xlink:href="#heart"></use></svg></a>
+                    <figure>
+                      <a href="index.html" title="${name}">
+                        <img src="${image}"  class="tab-image">
+                      </a>
+                    </figure>
+                    <h3>${name}</h3>
+                    <span class="qty">1 Unit</span><span class="rating"><svg width="24" height="24" class="text-primary"><use xlink:href="#star-solid"></use></svg> ${rating}</span>
+                    <span class="price">${price}</span>
+                    <div class="d-flex align-items-center justify-content-between">
+                      <div class="input-group product-qty">
+                          <span class="input-group-btn">
+                              <button type="button" class="quantity-left-minus btn btn-danger btn-number" data-type="minus">
+                                <svg width="16" height="16"><use xlink:href="#minus"></use></svg>
+                              </button>
+                          </span>
+                          <input type="text" id="quantity" name="quantity" class="form-control input-number" value="1">
+                          <span class="input-group-btn">
+                              <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus">
+                                  <svg width="16" height="16"><use xlink:href="#plus"></use></svg>
+                              </button>
+                          </span>
                       </div>
+                      <button type="button" onclick="handleAddtoCart(${id})" class="nav-link">Add to Cart <iconify-icon icon="uil:shopping-cart"></button>
+                    </div>
+                </div>
         `
         product_grid.appendChild(col);
     });
 });
+
+const handleAddtoCart = 
 
 getProducts();
